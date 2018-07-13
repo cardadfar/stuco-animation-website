d2ff156968ab5d26620f8104ee81742e5fb05856
Vicmart <vdadfar@optimum.net>
2018-07-12 23:45:02 -0400

    second commit

--- interactions.js
+++ interactions.js
@@ -6,3 +6,11 @@ $("#overview").click(function() {
     window.location.href = 'index.html';
 });
 
+$("#calendar").click(function() {
+    window.location.href = 'calendar.html';
+});
+
+$("#assignments").click(function() {
+    window.location.href = 'assignments.html';
+});
+
